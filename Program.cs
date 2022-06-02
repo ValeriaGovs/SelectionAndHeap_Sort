86bb4f57cb890c97d24af933c76d5cc98d900768
lera <lera@rbi.ru>
2022-06-02 17:57:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SelectionAndHeap_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] n = new int[] {1,16,13,5,3,9,5,3,4 };
+
+            int n = 10;
+
+            for (int i = 0; i < 6; i++)
+            {
+                n = n * 10;
+
+                TimeTest(n);
+            }
+
+
+
+        }
+
+        private static void TimeTest(int n)
+        {
+            Random r = new Random();
+            var unsort = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                unsort[i] = r.Next(0, n);
+            }
+
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //Sort.SelectionSort(unsort);
+            Sort.HeapSort(unsort);
+
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+            Console.WriteLine("RunTime {0} {1} = {2}", "HeapSort", unsort.Length, elapsedTime);
+
+            /*
+            foreach (int i in unsort)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine($"");
+            */
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionAndHeap_Sort
+{
+    class Sort
+    {
+
+
+        public static void HeapSort(int[] mass)
+        {
+
+            for (int root = mass.Length / 2 - 1; root >= 0; root--)
+                heapify(root, mass.Length, mass);
+
+            for (int j = mass.Length - 1; j > 0; j--)
+            {
+                swap(0, j, mass);
+                heapify(0, j, mass);
+
+            }
+
+        }
+
+        private static void heapify(int root, int size, int[] mass) // moveMaxToRoot
+        {
+            int L = 2 * root + 1;
+            int R = 2 * root + 2;
+            int X = root;
+            if (L < size && mass[L] > mass[X]) X = L;
+            if (R < size && mass[R] > mass[X]) X = R;
+            if (X == root) return;
+            swap(X, root, mass);
+            heapify(X, size, mass);
+        }
+
+
+
+        public static void SelectionSort(int[] mass)
+        {
+
+
+            int indMax = FindMax(mass, mass.Length);
+            for (int i = mass.Length - 1; i >= 0; i--)
+            {
+                swap(indMax, i, mass);
+                indMax = FindMax(mass, i);
+            }
+
+
+        }
+
+        private static void swap(int indMax, int i, int[] mass)
+        {
+            int k = mass[i];
+            mass[i] = mass[indMax];
+            mass[indMax] = k;
+        }
+
+        private static int FindMax(int[] mass, int j)
+        {
+            int max = 0;
+            int ind_max = 0;
+            for (int i = 0; i < j; i++)
+            {
+                if (mass[i] > max)
+                {
+                    ind_max = i;
+                    max = mass[i];
+                }
+            }
+            return ind_max;
+        }
+    }
+}
+
